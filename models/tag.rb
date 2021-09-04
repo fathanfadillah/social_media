1934e5f194ba9dcceb28904b9b326743c97da5df
fathanfadillah <fathan17698@gmail.com>
2021-09-05 02:39:26 +0700

    extract word from text

--- models/tag.rb
+++ models/tag.rb
@@ -1,14 +1,26 @@
+require_relative "../database/db_connector.rb"
+require_relative "../function/extract_word.rb"
+
 class Tag
-    def self.findTags(param)
+    def self.findTags(params)
         
     end
     
     def all
     end
     
-    def get(param)
+    def get(params)
     end 
 
-    def save(param)
+    def self.save(params)
+        tags = findTags(params)
+        client = create_db_client
+        tags.each do |tag|
+            puts tag
+            client.query("INSERT INTO tags (tag) values('#{@username}','#{@email}','#{@bio}')")
+        end 
     end 
 end 
+
+input = "user can see the list #of trending #hashtags."
+Tag.save(input)
